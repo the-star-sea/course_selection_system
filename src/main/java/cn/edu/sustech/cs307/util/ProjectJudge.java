b6a8008cbd29efa17192fb20f324710e0ac2afd6
sai-01 <11911635@mail.sustech.edu.cn>
2021-06-12 01:40:12 +0800

    hi

--- src/main/java/cn/edu/sustech/cs307/util/ProjectJudge.java
+++ src/main/java/cn/edu/sustech/cs307/util/ProjectJudge.java
@@ -55,30 +55,29 @@ public final class ProjectJudge {
                     .filter(it -> searchCourseExpected.get(it).equals(searchCourseResult.get(it))).count());
             for(int i=0;i<searchCourseExpected.size();i++){
                 if(!searchCourseExpected.get(i).equals(searchCourseResult.get(i))){
-
-                    for(int j=6;j<searchCourseExpected.get(i).size();j++){
-                  System.out.println(searchCourseExpected.get(i).get(j).course.equals(searchCourseResult.get(i).get(j).course));
-                        System.out.println(searchCourseExpected.get(i).get(j).section.equals(searchCourseResult.get(i).get(j).section));
-                        System.out.println(searchCourseExpected.get(i).get(j).sectionClasses.equals(searchCourseResult.get(i).get(j).sectionClasses));
-                        CourseSectionClass c1= (CourseSectionClass) searchCourseExpected.get(i).get(j).sectionClasses.toArray()[0];
-                        CourseSectionClass c2=(CourseSectionClass)searchCourseResult.get(i).get(j).sectionClasses.toArray()[0];
-                        System.out.println(c1.id==c2.id);
-                        System.out.println(c1.classEnd==c2.classEnd);
-                        System.out.println(c1.location==c2.location);
-                        System.out.println(c1.dayOfWeek==c2.dayOfWeek);
-                        System.out.println(c1.instructor.equals(c2.instructor));
-                        System.out.println(c1.weekList.equals(c2.weekList));
-                        c1= (CourseSectionClass) searchCourseExpected.get(i).get(j).sectionClasses.toArray()[1];
-                         c2=(CourseSectionClass)searchCourseResult.get(i).get(j).sectionClasses.toArray()[1];
-                        System.out.println(c1.id==c2.id);
-                        System.out.println(c1.classEnd==c2.classEnd);
-                        System.out.println(c1.location==c2.location);
-                        System.out.println(c1.dayOfWeek==c2.dayOfWeek);
-                        System.out.println(c1.instructor.equals(c2.instructor));
-                        System.out.println(c1.weekList.equals(c2.weekList));
+                    for(int j=0;j<searchCourseExpected.get(i).size();j++){
+//                  System.out.println(searchCourseExpected.get(i).get(j).course.equals(searchCourseResult.get(i).get(j).course));
+//                        System.out.println(searchCourseExpected.get(i).get(j).section.equals(searchCourseResult.get(i).get(j).section));
+//                        System.out.println(searchCourseExpected.get(i).get(j).sectionClasses.equals(searchCourseResult.get(i).get(j).sectionClasses));
+//                        CourseSectionClass c1= (CourseSectionClass) searchCourseExpected.get(i).get(j).sectionClasses.toArray()[0];
+//                        CourseSectionClass c2=(CourseSectionClass)searchCourseResult.get(i).get(j).sectionClasses.toArray()[0];
+//                        System.out.println(c1.id==c2.id);
+//                        System.out.println(c1.classEnd==c2.classEnd);
+//                        System.out.println(c1.location==c2.location);
+//                        System.out.println(c1.dayOfWeek==c2.dayOfWeek);
+//                        System.out.println(c1.instructor.equals(c2.instructor));
+//                        System.out.println(c1.weekList.equals(c2.weekList));
+//                        c1= (CourseSectionClass) searchCourseExpected.get(i).get(j).sectionClasses.toArray()[1];
+//                         c2=(CourseSectionClass)searchCourseResult.get(i).get(j).sectionClasses.toArray()[1];
+//                        System.out.println(c1.id==c2.id);
+//                        System.out.println(c1.classEnd==c2.classEnd);
+//                        System.out.println(c1.location==c2.location);
+//                        System.out.println(c1.dayOfWeek==c2.dayOfWeek);
+//                        System.out.println(c1.instructor.equals(c2.instructor));
+//                        System.out.println(c1.weekList.equals(c2.weekList));
                     }
-                    testSearchCourse(searchCourseParams.get(i));
-                }
+                   testSearchCourse(searchCourseParams.get(i));
+               }
             }
         }
         return result;
